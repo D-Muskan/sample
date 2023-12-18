660d43de9307db70f9afcaf97a979bbbca7c17b4
D-Muskan <soutien.art@gmail.com>
2023-12-18 18:19:17 +0530

    new landing page

---
+++ landing-page7/script.js
@@ -0,0 +1,30 @@
+gsap.from(".img1", {
+    duration: 1,
+    opacity:0,
+    delay:0.4,
+    y:50
+}
+)
+
+gsap.from(".img2", {
+    duration: 1,
+    opacity:0,
+    delay:0.4,
+    x:50
+}
+)
+gsap.from(".img3", {
+    duration: 1,
+    opacity:0,
+    delay:0.4,
+    y:-50
+}
+)
+
+gsap.from(".main h1", {
+    duration: 1,
+    opacity:0,
+    delay:1,
+ 
+}
+)
