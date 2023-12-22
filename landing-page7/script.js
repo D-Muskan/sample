2a584eb420ebf93cf93a22629c75dfa4f3ef615c
D-Muskan <soutien.art@gmail.com>
2023-12-22 20:09:12 +0530

    gsap annimation done

--- landing-page7/script.js
+++ landing-page7/script.js
@@ -1,3 +1,5 @@
+
+
 gsap.from(".img1", {
     duration: 1,
     opacity:0,
@@ -28,3 +30,15 @@ gsap.from(".main h1", {
  
 }
 )
+
+gsap.from(".page2 h5, .page2 h1, .page2 .about-us",{
+
+    opacity:0,
+    stagger:0.5,
+    scrollTrigger:{
+        trigger:".page2 h5",
+        scroller:"body",
+        start:"top 60%",
+        
+    }
+})
